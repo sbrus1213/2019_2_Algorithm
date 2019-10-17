c65b7200720d371fa938d097d555f9ce80a577a5
sbrus1213 <rlgus1590@gmail.com>
2019-10-16 20:49:32 -0700

    second assignment => delete test case & add function in make_input program(increase/decrease mode)

--- assignment2/input/make_input.cpp
+++ assignment2/input/make_input.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
 #include <cstring>
 #include <cstdio>
 #include <cstdlib>
@@ -11,16 +13,30 @@ int main(int argc, char* argv[]) {
 	int min_val = atoi(argv[3]);
 	int max_val = atoi(argv[4]);
 	int seed = atoi(argv[5]);
+	int order = 0;
+	if(argc == 7) order = atoi(argv[6]);
+	vector<int> arr(n);
 
 	srand(seed);
 
 	freopen(file_name, "w", stdout);
 
-	cout << n << ' ';
-	for (int i = 0; i < n; ++i) {
-		int x = rand() % (max_val - min_val + 1) + min_val;
-		cout << x << ' ';
+	for (int i = 0; i < n; ++i)
+		arr[i] = rand() % (max_val - min_val + 1) + min_val;
+	
+	switch(order) {
+	case 0:
+		break;	
+	case 1: 
+		sort(arr.begin(), arr.end(), less<int>());
+		break;
+	case -1:
+		sort(arr.begin(), arr.end(), greater<int>());	
 	}
+
+	cout << n << ' ';
+	for(int i = 0;i < n;++i)
+		cout<<arr[i] <<' ';
 
 	return 0;
 }
