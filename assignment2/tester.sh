4da95858605b2af8ada1b1da1feff5acae0a0577
sbrus1213 <rlgus1590@gmail.com>
2019-10-23 17:20:25 -0700

    tester 파일 수정 및 압축

--- assignment2/tester.sh
+++ assignment2/tester.sh
@@ -1,12 +1,17 @@
 INPUTS=$(ls *_input.txt | grep -v "result")
 rm -f result*
-echo "Start testing..."
-for input in $INPUTS
-do
-	./$1 $input 1
-	./$1 $input 2
-	./$1 $input 3
-	./$1 $input 4
-	echo "make result file for "$input
-done
-echo "Finish testing!!!"
+if [ $# -ne 0 ]
+then
+	echo "Start testing..."
+	for input in $INPUTS
+	do
+		./$1 $input 1 
+		./$1 $input 2 
+		./$1 $input 3 
+		./$1 $input 4 
+		echo "make result file for "$input
+	done
+	echo "Finish testing!!!"
+else 
+	echo "please write [binary file name]"
+fi
